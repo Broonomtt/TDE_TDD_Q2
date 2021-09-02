5b55e350deb44cc0b019e41edadb7263bb3aa33f
Bruno <bruno_motta@outlook.com>
2021-09-02 11:07:22 -0300

    Update calcmult.py

--- calcmult.py
+++ calcmult.py
@@ -1,2 +1,14 @@
-Def mult ():
-  pass
+def mult (n1, n2):
+  
+  if num_true(n1) and num_true(n2):
+     return float(n1) * float(n2)
+  else:
+     return None
+
+    
+def num_true(s):
+  try:
+    float(s)
+    return True
+  except ValueError:
+    return False
