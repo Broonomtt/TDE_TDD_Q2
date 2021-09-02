c7e78706f4a6ce6bb7fe2736a7a15cb7fbc0124a
Bruno <bruno_motta@outlook.com>
2021-09-02 11:10:03 -0300

    Update test_pytestes.py

--- test_pytestes.py
+++ test_pytestes.py
@@ -3,5 +3,5 @@ from calcmult import mult
 
 
 @pytest.mark.parametrize('n1,n2,produto', [(0, 0, 0), (-2,-5,10), (-2, 7,-14), (7.5, 5.4,40.5), (-1.0, -2.0,2.0), ('DOIS', 2, None)])
-def test_soma(n1,n2, produto):
-    assert soma(n1,n2) == produto
+def test_mult(n1,n2, produto):
+    assert mult(n1,n2) == produto
